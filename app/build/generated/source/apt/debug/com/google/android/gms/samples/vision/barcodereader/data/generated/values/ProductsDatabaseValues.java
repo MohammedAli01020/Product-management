1b76900a591cb1a7f974834cccfe356d1fb7040c
Mohamed Ali <mohanader2244@gmail.com>
2018-10-07 13:45:31 +0200

    second commit

--- app/build/generated/source/apt/debug/com/google/android/gms/samples/vision/barcodereader/data/generated/values/ProductsDatabaseValues.java
+++
@@ -1,7 +0,0 @@
-package com.google.android.gms.samples.vision.barcodereader.data.generated.values;
-
-public class ProductsDatabaseValues {
-  public ProductValuesBuilder forProduct() {
-    return new ProductValuesBuilder();
-  }
-}
